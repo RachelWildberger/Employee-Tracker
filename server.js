d441af15aa83212e50653f3778618bb9ec8f334b
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-02-07 14:27:55 -1000

    added dotenv package

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consTable = require('console.table');
+require('dotenv').config()
 
 const connection = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employee_tracker_db'
     },
-    console.log(`Connected to the employee_tracker_db.`)
+    console.log(`Connected to the Employee Database.`)
     
     );
