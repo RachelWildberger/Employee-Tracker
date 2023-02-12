fb5195edd866ff6b73a50c0a8a6b8a5b0448a280
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2023-02-12 13:47:05 -1000

    able to view all employees, view employees by dep

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,30 +10,30 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 110000, 1),
        ("Web Developer", 80000, 1),
        ("Program Manager", 115000, 1),
-       ("Systems Administrator", 84800, 1),
        ("Sales Manager", 95000, 2),
        ("Sales Development Rep", 80000, 2),
-       ("Project Manager", 71000, 3),
        ("Creative Director", 110000, 3),
        ("Graphic Designer", 70000, 3),
        ("Accounts Manager", 75000, 4),
-       ("Growth Marketing", 75000, 4),
-       ("Analytics Lead", 80000, 4),
        ("Financial analyst", 95500, 5),
        ("Legal Operations Manager", 88000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Will", "Johnson", 010, 012),
-       ("Kat", "Walsh", 011, 012),
-       ("Simon", "Mitchell", 012, NULL),
-       ("Austin", "Alexander", 013, 012),
-       ("Christina", "Nash", 014, NULL),
-       ("Danica", "O'Neil", 015, 014),
-       ("Peter", "Gibbs", 016, NULL),
-       ("Alison", "Riley", 017, 016),
-       ("Chris", "Bauer", 018, 017),
-       ("Sophia", "Sheridan", 019, NULL),
-       ("Bonnie", "Mullins", 020, 019 ),
-       ("Tyler", "Taylor", 021, NULL),
-       ("Matt", "Cristiano", 022, NULL),
-       ("Panda", "Greenberg", 023, NULL);
+VALUES ("Simon", "Mitchell", 3, NULL),
+       ("Austin", "Alexander", 4, NULL),
+       ("Danica", "O'Neil", 6, NULL),
+       ("Alison", "Riley", 8, NULL),
+       ("Chris", "Bauer", 9, NULL),
+       ("Sophia", "Sheridan", 10, NULL),
+       ("Matt", "Cristiano", 9, NULL);
+
+SELECT * FROM employee;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Will", "Johnson", 1, 3),
+     ("Kat", "Walsh", 2, 3),
+       ("Christina", "Nash", 5, 4),
+       ("Peter", "Gibbs", 7, 6),
+       ("Bonnie", "Mullins", 7, 6),
+      ("Tyler", "Taylor", 5, 4),
+     ("Panda", "Greenberg", 1, 3);
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const consTable = require("console.table");
+require("console.table");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'employee_tracker_db'
-  });
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to the Employee Database!!!!!")
-    startPrompt();
 });
 
-const startPrompt = () => {
+db.connect(function (err) {
+    if (err) throw err;
+    console.log(`\n`);
+    console.log('\x1b[33m Connected to the Employee Database! \x1b[0m');
+    console.log(`\n`);
+    employeePrompt();
+});
+
+const employeePrompt = () => {
     inquirer.prompt([
         {
             type: "list",
@@ -24,54 +26,185 @@ const startPrompt = () => {
             message: "What would you like to do?",
             choices: [
                 "View All Employees",
-                "Add Employee",
-                "View All Roles",
+                "View Employees by Department",
                 "Add Role",
-                "View All Departments",
-                "Add Department",
+                "Add Employee",
+                "Update Employee",
+                "Remove Employee",
                 "Exit"]
         }
     ])
         .then((response) => {
             switch (response.userChoice) {
                 case "View All Employees":
-                    viewEmployee();
+                    viewAllEmployees();
                     break;
-                case "Add Employee Role":
-                    addEmployee();
-                    break;
-                case "View All Roles":
-                    viewRoles();
+                case "View Employees by Department":
+                    employeeByDep();
                     break;
                 case "Add Role":
                     addRole();
                     break;
-                case "View All Departments":
-                    viewDepartments();
+                case "Add Employee":
+                    addEmployee();
                     break;
-                case "Add Department":
-                    addDepartment();
+                case "Update Employee":
+                    updateEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
                 case "Exit":
-                    connection.end();
+                    db.end();
                     break;
             }
         })
         .catch((err) => {
-            if(err)throw err;
+            if (err) throw err;
         })
 };
 
-const viewEmployee = () => {
-    console.log("Viewing all employees\n");
+const viewAllEmployees = () => {
+    const sql = `SELECT employee.first_name, employee.last_name, role.title, department.department_name, role.salary
+    FROM employee LEFT JOIN role ON employee.role_id = role.id 
+    LEFT JOIN department ON role.department_id = department.id`;
 
-    const query = 
-    `SELECT 
-        employee.id,
-        employee.first_name,
-        employee.last_name,
-        role.title,
-        department.department_name
-
-        
-        `
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log(`\n`);
+        console.log('\x1b[33m Viewing All Employees: \x1b[0m');
+        console.log(`\n`);
+        console.table(rows);
+        employeePrompt();
+    })
 };
+
+const employeeByDep = () => {
+    const sql = `SELECT department.department_name, employee.first_name, employee.last_name, role.title, role.salary 
+    FROM employee LEFT JOIN role ON employee.role_id = role.id 
+    LEFT JOIN department ON role.department_id = department.id;`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log(`\n`);
+        console.log('\x1b[33m Viewing Employees by Department: \x1b[0m');
+        console.log(`\n`);
+        console.table(rows);
+        employeePrompt();
+    })
+};
+
+const addRole = () => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, async (err, rows) => {
+        if (err) throw err;
+        rows = rows.map(row => {
+            return {
+                key: row.id,
+                value: row.department_name,
+            }
+        });
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "roleTitle",
+                message: "What is the role title?",
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "What is the role salary?",
+            },
+            {
+                type: "list",
+                name: "roleDepartment",
+                message: "What is the role department?",
+                choices: rows,
+            },
+            
+        ])
+        console.log(`\n`);
+        console.log('\x1b[33m New Role Has Been Added. \x1b[0m');
+        console.log(`\n`);
+        console.table();
+        employeePrompt();
+    })
+};
+
+const addEmployee= () => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, async (err, rows) => {
+        if (err) throw err;
+        rows = rows.map(row => {
+            return {
+                key: row.id,
+                value: row.department_name,
+            }
+        });
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "employeeName",
+                message: "What is the employees name?",
+            },
+            {
+                type: "list",
+                name: "employeeDepartment",
+                message: "What is the employees department?",
+                choices: rows,
+            },
+            {
+                type: "input",
+                name: "employeeRole",
+                message: "What is the employees role?",
+            },
+            {
+                type: "input",
+                name: "employeeSalary",
+                message: "What is the employees salary?",
+            },
+            {
+                type: "input",
+                name: "employeeId",
+                message: "What is the employees id?",
+            },
+            {
+                type: "input",
+                name: "employeeManager",
+                message: "Who is the employees manager?",
+            },
+            
+        ])
+        console.log(`\n`);
+        console.log('\x1b[33m New employee Has Been Added. \x1b[0m');
+        console.log(`\n`);
+        console.table();
+        employeePrompt();
+    })
+};
+
+// const updateEmployee = () => {
+//     const sql = ``;
+
+//     db.query(sql, (err, rows) => {
+//         if (err) throw err;
+//         console.log('Data received from DB:');
+//         console.table(rows);
+//         employeePrompt();
+//     })
+
+// };
+
+// const removeEmployee = () => {
+//     const sql = ``;
+
+//     db.query(sql, (err, rows) => {
+//         if (err) throw err;
+//         console.log('Data received from DB:');
+//         console.table(rows);
+//         employeePrompt();
+//     })
+
+// };
